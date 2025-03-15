4faecd4dc86b771674e23a34a1c8e2e8c1e3ffcc
chaeho5 <dkscogh1@gmail.com>
2025-03-15 23:09:00 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,11 +1,7 @@
 a = int(input())
 b = int(input())
 
-b1 = b%10
-b10 = (b//10)%10
-b100 = b//100
-
-print(a*b1)
-print(a*b10)
-print(a*b100)
+print(a*(b%10))
+print(a*((b//10)%10))
+print(a*(b//100))
 print(a*b)
