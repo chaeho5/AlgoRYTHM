e14116f1bf7461d1ef0f860009aa7f1a9a7246a5
chaeho5 <dkscogh1@gmail.com>
2025-03-17 17:19:00 +0900

    [Gold V] Title: 하노이 탑, Time: 436 ms, Memory: 149720 KB -BaekjoonHub

---
+++ 백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+def move(n, x, y):
+    if n > 1:        
+        move(n - 1, x, 6-x-y)
+    print(x, y)
+    if n > 1:
+        move(n - 1, 6-x-y, y)
+     
+n = int(input())
+print((2**n)-1)
+if n <= 20:
+    move(n , 1, 3)
+
+
