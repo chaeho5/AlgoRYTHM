47ed7f87accdad9e45b1ffd3a6d1171ccdb2d37d
chaeho5 <dkscogh1@gmail.com>
2025-03-18 15:41:48 +0900

    [Bronze II] Title: 수 정렬하기, Time: 96 ms, Memory: 109412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,11 +1,14 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 numbers = []
 
 for _ in range(n):
     numbers.append(int(input()))
+# 2750번에선 sort사용
+s_numbers = sorted(numbers)
 
-numbers.sort()
-
-for number in numbers:
+for number in s_numbers:
     print(number)
