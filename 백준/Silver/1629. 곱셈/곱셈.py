48e314d0e227f19aa1735804f71be19bee971ec6
chaeho5 <dkscogh1@gmail.com>
2025-03-25 14:11:20 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,16 +1,2 @@
-def jegob(a, b, c):
-    result = 1
-    a = a % c
-
-    while b > 0:
-
-        if b % 2 == 1:
-            result = (result * a) % c
-        
-        a = (a * a) % c
-        b //= 2
-
-    return result
-
 a, b, c = map(int, input().split())
-print(jegob(a, b, c))
+print(pow(a, b, c))
