5cf825398225983a4d70015f6b021026c15ebfe4
chaeho5 <dkscogh1@gmail.com>
2025-04-04 02:43:51 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,14 +1,19 @@
-#저장할 배열 만들고
 n = int(input())
 
-memoi = [0] * (n + 1)
+memoi =[0] * (n + 1)
 
-def fibo(n):
-    if n == 1 or n == 2:
-        return 1
-    if memoi[n] != 0: #memoi배열이 0이 아니라면, 이미 계산했던 과정이라는 뜻이므로 저장된 값 반환
-        return memoi[n]
-    memoi[n] = fibo(n - 1) + fibo(n - 2)
-    return memoi[n]
+if n == 0:
+    print(0)  # F(0) = 0인 경우를 별도로 처리
+    exit()
+elif n == 1:
+    print(1)  # F(1) = 1인 경우를 별도로 처리
+    exit()
 
-print(fibo(n))
+
+memoi[1] = 1
+memoi[2] = 1
+
+for i in range(3, n + 1):
+    memoi[i] = memoi[i - 1] + memoi[i - 2]
+
+print(memoi[n])
