10157cf5e3dea453c7e2ab42f6c23ce51e6f6e2a
chaeho5 <dkscogh1@gmail.com>
2025-04-07 01:02:31 +0900

    [Gold V] Title: LCS, Time: 416 ms, Memory: 57628 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,10 +7,8 @@ m = list(input().strip())
 lcs = [[0] * (len(m)+1) for _ in range(len(n) + 1)]
 
 for i in range(1, len(n)+1):
-    for j in range(1, len(m)+1):
-        if i == 0 or j == 0:
-            lcs[i][j] = 0
-        elif n[i - 1] == m[j - 1]:
+    for j in range(1, len(m)+1):     
+        if n[i - 1] == m[j - 1]:
             lcs[i][j] = lcs[i-1][j-1] + 1
         else:
             lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1])
