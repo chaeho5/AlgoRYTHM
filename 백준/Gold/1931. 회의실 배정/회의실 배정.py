c0f66d713e45eed9a38382e5541ba5d665a4a18f
chaeho5 <dkscogh1@gmail.com>
2025-04-09 23:53:18 +0900

    [Gold V] Title: 회의실 배정, Time: 336 ms, Memory: 123148 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+time = []
+for _ in range(1, n + 1):
+    start, end = list(map(int, input().split()))
+    sigan = start, end
+    time.append(sigan)
+
+time.sort(key=lambda x : (x[1], x[0]))
+count = 1
+end = time[0][1]
+for i in range(1, n):
+    if time[i][0]>=end:
+        end = time[i][1]
+        count += 1
+
+print(count)
+          
