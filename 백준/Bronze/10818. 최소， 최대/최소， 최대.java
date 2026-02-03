733685457ff1b0ce0f4fc98e7d913c8a48b01596
chaeho <dkscogh1@gmail.com>
2026-02-03 20:42:39 +0900

    [Bronze III] Title: 최소, 최대, Time: 368 ms, Memory: 116616 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,33 +1,22 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
-        int currentNum = Integer.parseInt(st.nextToken());
-        int max = currentNum;
-        int min = currentNum;
+        for(int i = 0; i < N; i++) {
+            int currentNum = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i < N; i++){
-            currentNum = Integer.parseInt(st.nextToken());
-
-            if(currentNum > max){
-                max = currentNum;
-            }
-
-            if(currentNum < min){
-                min = currentNum;
-            }
+            min = Math.min(min, currentNum);
+            max = Math.max(max, currentNum);
         }
-
         System.out.println(min + " " + max);
     }
-
 }
