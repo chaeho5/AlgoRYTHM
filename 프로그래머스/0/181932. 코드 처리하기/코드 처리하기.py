dac297e5cbca6043d72b8fc38e1e3e3f29190a9f
chaeho <dkscogh1@gmail.com>
2026-03-06 10:37:20 +0900

    [level 0] Title: 코드 처리하기, Time: 13.02 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -0,0 +1,22 @@
+def solution(code):
+    mode = 0
+    ret =''
+    
+    for i in range(len(code)):
+        if mode == 0 :
+            if code[i] == "1":
+                mode = 1
+            elif i % 2 == 0:
+                ret += code[i]
+        elif mode == 1 :
+            if code[i] == '1':
+                mode = 0
+            elif i % 2 == 1:
+                ret += code[i]
+        
+    if ret == '':
+        return 'EMPTY'
+    else:
+        return ret
+        
+            
