7019b4fa5c23e5b3a73d76fea1268e604c16e95b
chaeho <dkscogh1@gmail.com>
2026-03-15 22:53:50 +0900

    [Bronze V] Title: 사칙연산, Time: 4 ms, Memory: 988 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	var A, B int
+	fmt.Fscan(reader, &A, &B)
+	fmt.Println(A + B)
+	fmt.Println(A - B)
+	fmt.Println(A * B)
+	fmt.Println(A / B)
+	fmt.Print(A % B)
+}
