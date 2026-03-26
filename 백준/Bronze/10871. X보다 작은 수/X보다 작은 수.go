d67db771d78b496a039ffe574a940163f4a17099
chaeho <dkscogh1@gmail.com>
2026-03-26 23:04:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 8 ms, Memory: 1192 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.go
@@ -0,0 +1,28 @@
+package main 
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+	var n, x int
+
+	fmt.Fscan(reader, &n, &x)
+
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+
+	for i := 0; i < n; i++ {
+		if a[i] < x {
+			fmt.Fprintf(writer,"%v ",a[i])
+		}
+	}
+}
