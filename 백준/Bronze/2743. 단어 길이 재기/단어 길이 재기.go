65f9cb32b81a72a24fb2878ef883bf364cfb0676
chaeho <dkscogh1@gmail.com>
2026-04-06 00:49:14 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 4 ms, Memory: 988 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+var writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+	defer writer.Flush()
+
+	var s string
+	fmt.Fscan(reader, &s)
+	fmt.Fprint(writer, len([]rune(s)))
+}
