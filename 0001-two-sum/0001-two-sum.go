1f2a31f0fc5219b7e805816482f4a049a319a432
chaeho <dkscogh1@gmail.com>
2026-07-01 00:02:26 +0900

    Time: 23 ms (17.8%), Space: 5.4 MB (86.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,7 +1,7 @@
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-    for i := 0; i < len(nums) - 1; i++{
+    for i := 0; i < len(nums); i++{
         for j := i + 1; j < len(nums); j++{
             if nums[i] + nums[j] == target{
                 return []int{i, j}
