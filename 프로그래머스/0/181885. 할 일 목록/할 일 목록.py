cfc1b835cecb566defb7befb09c2deda021ea082
chaeho <dkscogh1@gmail.com>
2026-07-18 22:55:17 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,9 @@
+def solution(todo_list, finished):
+    result = []
+    for i in range(len(todo_list)):
+        if not finished[i]:
+            result.append(todo_list[i])
+                
+    return result
+                    
+            
